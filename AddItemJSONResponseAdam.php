99968bcd7c500735d722d00143075848eaf1211b
Dennis Pavlyuk <37779084+dpsoccerdude101@users.noreply.github.com>
2020-10-28 21:48:57 -0400

    Create AddItemJSONResponseAdam.php
    Adam's update wasnt working

---
+++ AddItemJSONResponseAdam.php
@@ -0,0 +1,45 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    exit;
+}
+session_start();
+require 'database.php';
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$data = json_decode($json);
+
+$Description=$data->Description;
+$Size=$data->Size;
+$Division=$data->Division;
+$Department=$data->Department;
+$Category=$data->Category;
+$ItemCost=$data->ItemCost;
+$ItemRetail=$data->ItemRetail;
+$ImageFileName=$data->ImageFileName;
+$VenId=(int)$data->VenId;
+
+$Error = "Error Inserting Data";
+
+
+$link = new mysqli("localhost", "root", "xXT@jyY2yg3P", "nannos_foods");
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+}
+
+$query = "INSERT INTO InventoryItem (Description, Size, Division, Department, Category, ItemCost, ItemRetail, ImageFileName, VendorId) 
+VALUES('$Description', '$Size', '$Division', '$Department', '$Category', '$ItemCost', '$ItemRetail', '$ImageFileName', '$VenId')";
+
+
+if($link->query($query) === TRUE) {
+    $result = '{"result": "success"}';
+    header('Content-Type: application/json, Access-Control-Allow-Origin : *');
+    echo json_encode($result);
+} else {
+    $result = '{"result": "failure"}';
+    header('Content-Type: application/json, Access-Control-Allow-Origin : *');
+    echo json_encode($result);
+    $_SESSION['error'] = $Error;
+}
+$link->close();
