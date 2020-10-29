3ce2483bd413552e82c95a0ff2e48b4d4989e4bb
collinfanning <cfann2@brockport.edu>
2020-10-29 11:14:44 -0400

    Adding my implementation for the backend

--- venModifyJSONResponseCollin.php
+++ venModifyJSONResponseCollin.php
@@ -1,3 +1,39 @@
 <?php
-    //Wanted to FTP this to the deployed server!
-    //adding another comment to make sure im using this git stuff properly.
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    exit;
+}
+session_start();
+require 'database.php';
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$data = json_decode($json);
+
+$VendorID = (int)$data->VendorID;
+
+
+$Error = "Error Inserting Data";
+
+
+$link = new mysqli("localhost", "root", "xXT@jyY2yg3P", "nannos_foods");
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+}
+
+$query = "SELECT * FROM Vendor WHERE VendorID='$VendorID'";
+$results = mysqli_query($link, $query);
+$rows = mysqli_num_rows($results);
+
+if($rows >= 1) {
+    $result = '{"result": "success"}';
+   // $vendorinfo = '{"VendorID": "}'
+    header('Content-Type: application/json, Access-Control-Allow-Origin : *');
+    echo json_encode($result);
+} else {
+    $result = '{"result": "failure"}';
+    header('Content-Type: application/json, Access-Control-Allow-Origin : *');
+    echo json_encode($result);
+    $_SESSION['error'] = $Error;
+}
+$link->close();
