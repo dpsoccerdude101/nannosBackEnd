8f120e6ecee095c07a5f2473cd2e97e9fe144dc6
aczy623 <62111741+aczy623@users.noreply.github.com>
2020-11-10 19:57:21 -0500

    deleteItem

---
+++ deleteItemJSONResponseAdam.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    exit;
+}
+session_start();
+require 'database.php';
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$data = json_decode($json);
+
+$ItemID = (int)$data->ItemID;
+
+
+$Error = "Error Inserting Data";
+
+
+$link = new mysqli("localhost", "root", "SsSMUeVf2nzj", "nannos_foods");
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+}
+
+$query = "UPDATE InventoryItem SET Status='Inactive' WHERE ItemId='$ItemID'";
+
+if($link->query($query) === TRUE) {
+    $result = '{"result": "success"}';
+    header('Content-Type: application/json, Access-Control-Allow-Origin : *');
+    echo json_encode($result);
+} else {
+    $result = '{"result": "failure"}';
+    header('Content-Type: application/json, Access-Control-Allow-Origin : *');
+    echo json_encode($result);
+    $_SESSION['error'] = $Error;
+}
+$link->close();
