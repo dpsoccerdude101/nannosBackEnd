d05f307a2f6a4b2f973a3dbb13050267d63c3806
stevenpolvino <spolv1@brockport.edu>
2020-11-12 13:56:55 -0500

    Created useless files, and test file

---
+++ testing/Test.php
@@ -0,0 +1,10 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase {
+
+
+
+}
---
+++ testing/sum.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = 5;
+$b = 6;
+
+function addNums() {
+	console.log(5 + 6);
+}
+
+?>
