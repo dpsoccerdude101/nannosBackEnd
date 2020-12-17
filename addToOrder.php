65f69c486aa022f8eb42f9f81f3431e19547e581
Mike_Kowulich <Mike_Kowulich@bitbucket.org>
2020-12-16 23:55:05 -0500

    Add to order

---
+++ addToOrder.php
@@ -0,0 +1,47 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    exit;
+}
+session_start();
+require 'database.php';
+
+$link = mysqli_connect("localhost", "root", "SsSMUeVf2nzj", "nannos_foods");
+if (!$link) {
+    die("Connection failed: " . mysqli_connect_error);
+}
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$data = json_decode($json, true);
+
+$OrderId;
+$resultOfInsert = TRUE;
+    foreach($data as $key => $value) {
+        $OrderId = $value["OrderId"];
+        $ItemId = $value["ItemId"];
+        $quantity = $value["quantity"];
+        $query = "INSERT INTO OrderDetail (OrderId, ItemId, QuantityOrdered) 
+        VALUES('$OrderId', '$ItemId', '$quantity')";
+        if(mysqli_query($link, $query)){
+            $resultOfInsert = TRUE;
+        }
+        else {
+            $resultOfInsert = FALSE;
+            break;
+        }
+    }
+
+$Error = "Error Inserting Data";
+
+if($resultOfInsert === TRUE) {
+    $result = '{"result": "success"}';
+    header('Content-Type: application/json, Access-Control-Allow-Origin : *');
+    echo json_encode($result);
+} else {
+    $result = "{\"result\": \"$query\"}";
+    header('Content-Type: application/json, Access-Control-Allow-Origin : *');
+    echo json_encode($result);
+    $_SESSION['error'] = $Error;
+}
+$link->close();
